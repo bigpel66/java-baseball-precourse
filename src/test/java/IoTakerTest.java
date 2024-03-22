d048021ac8ee03256d241a09504b35fed2eba5ad
bigpel66 <bigpel66@icloud.com>
2024-03-22 16:07:54 +0900

    test(IoTakerTest): checkNonNumericInput 메서드 작성 - 임의의 문자열이 제공되었을 때 숫자로 읽으려는 경우의 예외를 확인

--- src/test/java/IoTakerTest.java
+++ src/test/java/IoTakerTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("입출력 담당 객체 테스트")
 class IoTakerTest {
@@ -36,6 +38,14 @@ class IoTakerTest {
         setInputStream("9");
         Scanner sc = new Scanner(System.in);
         assertThat(sc.nextInt()).isEqualTo(9);
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 값 입력 확인")
+    void checkNonNumericInput() {
+        setInputStream("string");
+        Scanner sc = new Scanner(System.in);
+        assertThatThrownBy(sc::nextInt).isInstanceOf(InputMismatchException.class);
     }
 
 }
