2c3524a304fe9e411e1d641196eeb606b99531e4
bigpel66 <bigpel66@icloud.com>
2024-03-23 01:46:18 +0900

    test(Answer): Answer 객체 로직 및 테스트 - grade 로직 작성 - containsDigit, matchDigit 접근 지정자 public -> private - containsDigit, matchDigit 작성 - Answer 객체 초기 상태 및 입력 값에 따른 Status 확인 테스트 - Answer 로직 동작 확인

--- src/main/java/Answer.java
+++ src/main/java/Answer.java
@@ -1,16 +1,38 @@
-public final class Answer {
+import java.util.HashSet;
+import java.util.Set;
 
+public final class Answer {
     private Status status;
 
-    public Answer() {
-        status = Status.newInstance();
+    private final String value;
+
+    private final Set<Integer> containSet = new HashSet<>();
+
+    public Answer(String input) {
+        value = input;
+        for (int i = 0; i < value.length(); i++) {
+            containSet.add(value.charAt(i) - '0');
+        }
+    }
+
+    private Answer() {
+        throw new AssertionError("정답 값이 없는 생성을 방지");
     }
 
     public static Answer of(String input) {
-        return new Answer();
+        return new Answer(input);
     }
 
     public Status grade(String guess) {
+        status = Status.newInstance();
+        for (int i = 0; i < guess.length(); i++) {
+            int digit = guess.charAt(i) - '0';
+            if (matchDigit(digit, i)) {
+                status.increaseStrike();
+            } else if (containsDigit(digit)) {
+                status.increaseBall();
+            }
+        }
         return status;
     }
 
@@ -18,12 +40,12 @@ public final class Answer {
         return status;
     }
 
-    public boolean containsDigit() {
-        return true;
+    private boolean containsDigit(int digit) {
+        return containSet.contains(digit);
     }
 
-    public boolean matchDigit() {
-        return true;
+    private boolean matchDigit(int digit, int index) {
+        return containsDigit(digit) && (value.charAt(index) - '0' == digit);
     }
 
 }
