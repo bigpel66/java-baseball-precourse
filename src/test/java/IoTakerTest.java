f9d86dafcf4e5fbb19576c9f103343b1453360f2
bigpel66 <bigpel66@icloud.com>
2024-03-22 16:02:51 +0900

    test(IoTakerTest): checkNumericInput 메서드 작성 - 임의의 숫자가 표준 입력으로 제공되었음을 테스트

--- src/test/java/IoTakerTest.java
+++ src/test/java/IoTakerTest.java
@@ -1,10 +1,14 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("입출력 담당 객체 테스트")
 class IoTakerTest {
@@ -24,6 +28,14 @@ class IoTakerTest {
 
     void setInputStream(String input) {
         System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+
+    @Test
+    @DisplayName("숫자 입력 확인")
+    void checkNumericInput() {
+        setInputStream("9");
+        Scanner sc = new Scanner(System.in);
+        assertThat(sc.nextInt()).isEqualTo(9);
     }
 
 }
